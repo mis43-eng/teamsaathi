292946c46834d12e6e761b2023040c25f468c6f0
mis43-eng <mis43@kanodiaindia.com>
2026-09-10 12:39:54 +0530

    Add files via upload

---
+++ api.js
@@ -0,0 +1,19 @@
+﻿const serverless = require('serverless-http');
+const { initDatabase } = require('../../database.js');
+const app = require('../../server.js');
+
+let isInitialized = false;
+let initPromise = null;
+
+const handler = serverless(app);
+
+module.exports.handler = async (event, context) => {
+  if (!isInitialized) {
+    if (!initPromise) {
+      initPromise = initDatabase();
+    }
+    await initPromise;
+    isInitialized = true;
+  }
+  return handler(event, context);
+};
